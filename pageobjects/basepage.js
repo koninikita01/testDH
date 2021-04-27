92960adf2ff9b524f901e4c38bc39b02fca91db6
koninikita01 <nikita.koni@credera.co.uk>
2021-04-27 18:55:04 +0100

    chnaged chrome path

--- pageobjects/basepage.js
+++ pageobjects/basepage.js
@@ -1,22 +1,21 @@
 const {Capabilities, until, By} = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
 let ch = require('selenium-webdriver/chrome');
+const chromedriver = require('chromedriver');  
+ch_options = new ch.setDefaultService(new ch.ServiceBuilder('node_modules\\chromedriver\\lib\\chromedriver\\chromedriver.exe').build());
+
+
 let options = new ch.Options().headless();
 options.addArguments('start-fullscreen');
 options.addArguments('disable-inforbars');
 options.addArguments('--headless');
 options.addArguments('--disable-extensions')
-options.addArguments('--remote-debugging-port=9222')  
-//options.setChromeBinaryPath('/usr/local/bin/chromeDriver');
-// options.setChromeBinaryPath('/github/home/Downloads/chromeDriver');
+options.addArguments('--remote-debugging-port=9222') ;
 
 const caps = new Capabilities();
 caps.setPageLoadStrategy("normal");
 let driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
 
-// var driver = manager.chromedriver().setup();
-//  var d = new manager.Builder().
- //new manager.chrome.browserVersion("90.0.4430.24").setup();
 class BasePage {
 constructor()
 {
@@ -33,5 +32,4 @@ async go_to_url(baseURL)
 
 module.exports = BasePage;
 
-//export PATH=$PATH:"/usr/local/share/chromedriver" 
 
