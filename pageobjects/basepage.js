b29f11d198334d550444a038ceac5e5f55ee50f1
koninikita01 <nikita.koni@credera.co.uk>
2021-04-27 10:48:46 +0100

    made changes in chrome pth

--- pageobjects/basepage.js
+++ pageobjects/basepage.js
@@ -4,13 +4,20 @@ var ch = require('selenium-webdriver/chrome');
 var options = new ch.Options();
 options.addArguments('start-fullscreen');
 options.addArguments('disable-inforbars');
-options.addArguments('headless');
+options.addArguments('--headless');
+options.addArguments('--disable-extensions')
+options.addArguments('--remote-debugging-port=9222')  
+options.setChromeBinaryPath('/usr/local/bin/chromeDriver');
+
+
 
 const caps = new Capabilities();
 caps.setPageLoadStrategy("normal");
 var driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
 
-
+// var driver = manager.chromedriver().setup();
+//  var d = new manager.Builder().
+ //new manager.chrome.browserVersion("90.0.4430.24").setup();
 class BasePage {
 constructor()
 {
@@ -26,3 +33,5 @@ async go_to_url(baseURL)
 
 
 module.exports = BasePage;
+
+//export PATH=$PATH:"/usr/local/share/chromedriver" 
