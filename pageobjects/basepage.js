1a52c0bc252d34f35bd1fb64c8966ccd5c2f5a35
koninikita01 <82101889+koninikita01@users.noreply.github.com>
2021-04-27 11:05:06 +0100

    Update basepage.js

--- pageobjects/basepage.js
+++ pageobjects/basepage.js
@@ -1,7 +1,7 @@
 const {Capabilities, until, By} = require('selenium-webdriver');
 var webdriver = require('selenium-webdriver');
-var ch = require('selenium-webdriver/chrome');
-var options = new ch.Options();
+let ch = require('selenium-webdriver/chrome');
+let options = new ch.Options().headless();
 options.addArguments('start-fullscreen');
 options.addArguments('disable-inforbars');
 options.addArguments('--headless');
@@ -11,7 +11,7 @@ options.setChromeBinaryPath('/github/home/Downloads/chromeDriver');
 
 const caps = new Capabilities();
 caps.setPageLoadStrategy("normal");
-var driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
+let driver = new webdriver.Builder().setChromeOptions(options).forBrowser('chrome').build();
 
 
 class BasePage {
