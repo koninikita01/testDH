c90ff1297b1bdf8e33902e2aa2205865f183cdb0
koninikita01 <nikita.koni@credera.co.uk>
2021-05-07 13:56:37 +0100

    aded

--- test/TC.test.js
+++ test/TC.test.js
@@ -1,38 +1,45 @@
 var webdriver = require('selenium-webdriver');
-var firefox = require('selenium-webdriver/firefox');
-let driver = webdriver;
+let firefox = require('selenium-webdriver/firefox');
+driver = new webdriver.Builder();
+require('jest');
+// require("geckodriver");
 // require('ms-chromium-edge-driver');
 const{Builder, By, findElement} = require('selenium-webdriver');
 const assert = require('assert');
 var Firefox_options = new firefox.Options();
 // const { installDriver } =require('ms-chromium-edge-driver');
 var edge = require('selenium-webdriver/edge');
-
 // const ff_binary = new firefox.Binary();
 
 
-jest.setTimeout(10000);
+jest.setTimeout(6000);
 
-beforeAll(async () => {
+
+beforeEach(async () => {
   let capabilities= webdriver.Capabilities;
+  console.log('beforeEach block');
+  
   switch (process.env.BROWSER) {
-   
-    case "edge": {
-
-      const driverPath = path.join(
-        __dirname,
-        "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
-      );
-      process.env.PATH = `${process.env.PATH};${driverPath};`;
-      capabilities = webdriver.Capabilities.ie();
-      capabilities.set("ignoreProtectedModeSettings", true);
-      capabilities.set("ignoreZoomSetting", true);
-      driver = await new webdriver.Builder()
-      .withCapabilities(capabilities)
-      .build();
-      break;
-
+       case "edge": {
+          const edgePaths = await installDriver();
+          const edgeOptions = new edge.Options();
+          edgeOptions.setEdgeChromium(true);
+          edgeOptions.setBinaryPath(edgePaths.browserPath);
+        //  edgeOptions.headless();
+        // edgeOptions.addArguments("headless"); 
  
+      // HACK: include IEDriver path by nuget
+      // const driverPath = path.join(
+      //   __dirname,
+      //   "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
+      // );
+      // process.env.PATH = `${process.env.PATH};${driverPath};`;
+     
+      // capabilities = webdriver.Capabilities.edge();
+      // capabilities.set("ignoreProtectedModeSettings", true);
+      // capabilities.set("ignoreZoomSetting", true);
+
+       break;
     }
     case "safari": {  
       capabilities = webdriver.Capabilities.safari();
@@ -40,18 +47,13 @@ beforeAll(async () => {
     }
 
     case "firefox": {
-      require("geckodriver");
-      console.log('in firefox');
-      Firefox_options.addArguments('-headless');
+     
+      // console.log('in firefox');
+      Firefox_options.addArguments('--headless');
       Firefox_options.addArguments('--disable-gpu');
       Firefox_options.addArguments('--window-size=1980,1200')
       capabilities = webdriver.Capabilities.firefox();
-      capabilities.set("firefoxOptions", {
-        args: [
-          "--headless"
-        ]
-      });
-      driver = await new webdriver.Builder().forBrowser ('firefox').setFirefoxOptions(Firefox_options)
+      driver = await new webdriver.Builder().forBrowser('firefox').setFirefoxOptions(Firefox_options).usingServer('http://localhost:4444/wd/hub')
       .withCapabilities(capabilities)
       .build();
       break;
@@ -63,40 +65,49 @@ beforeAll(async () => {
       capabilities = webdriver.Capabilities.chrome();
       capabilities.set("chromeOptions", {
         args: [
-          "--headless",
+          // "--headless",
           "--no-sandbox",
           "--disable-gpu",
           "--window-size=1980,1200"
         ]
       });
-      driver = await new webdriver.Builder().forBrowser ('chrome')
+      driver = await new webdriver.Builder().forBrowser('chrome')
       .withCapabilities(capabilities)
       .build();
       break;
     }
 
   }
-
   
+      // driver = await new webdriver.Builder()
+      // .forBrowser('MicrosoftEdge')
+      // .withCapabilities(capabilities)
+      // .setEdgeOptions(edgeOptions)
+      // .setEdgeService(new edge.ServiceBuilder(edgePaths.driverPath))
+      // .build();
+
+
  });
 
-afterAll(async () => {
+afterEach(async () => {
+  console.log('afterEach block');
   await driver.quit();
-});
+},2000);
 
 
-it("launch DH and assert username text field", async () => {
+test("launch DH and assert username text field", async () => {
   await driver.get('https://dighybprstaging.z6.web.core.windows.net/');
   var username =  await driver.findElement(By.id('username'));
   await username.sendKeys('dh.1@client');
+  console.log('found username');
   var val =  await driver.findElement(By.id('username')).getAttribute("value");
   assert.strictEqual(val, 'dh.1@client');
 });
 
-function handleFailure(err, driver) {
-  console.error('Something went wrong!\n', err.stack, '\n');
-  driver.quit();
-}
+// function handleFailure(err, driver) {
+//   console.error('Something went wrong!\n', err.stack, '\n');
+//   driver.quit();
+// }
 
 
 //reporters: [
