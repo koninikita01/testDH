6ecbf619adf66f886172483acce71812820a8109
koninikita01 <nikita.koni@credera.co.uk>
2021-05-13 12:34:10 +0100

    using artifact

--- jest.conf.js
+++ jest.conf.js
@@ -1,18 +1,18 @@
 module.exports = {
     roots: ["<rootDir>"],
+    reporters: [
+      "default",
+      [jest-html-reporters, {
+        filename: "./TestReport.html",
+       "expand": true,
+        "openReport": true
+      }]
+    ],
     transform: {
       "^.+\\.tsx?$": "ts-jest"
     },
     testRegex: "(/(TC|e2e)/.*|(\\.|/))\\.test\\.js?$",
     coveragePathIgnorePatterns: ["dist", "node_modules"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    reporters: [
-      "default",
-      ["jest-html-reporters", {
-        "publicPath": "./html-report",
-        "filename": "TestReport.html",
-        "expand": true,
-        "openReport": true
-      }]
-    ]
+    
   };
